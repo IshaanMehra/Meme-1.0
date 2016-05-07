017e4bb0729b94411801e00a1157888a07d4d235
Ishaan Mehra <ishaanmehra@ishaans-macbook-pro.local>
2016-05-08 01:31:43 +0530

    Initial Commit

---
+++ Meme1.0/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Meme1.0
+//
+//  Created by Ishaan Mehra on 08/05/16.
+//  Copyright © 2016 Ishaan Mehra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
